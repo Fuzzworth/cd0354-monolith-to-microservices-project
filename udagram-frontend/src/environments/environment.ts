f83f76103c4c3725d933c428777bebbe285fab5f
Nyangasi Mhozya <nyangasi.m@gmail.com>
2022-11-15 00:04:22 +0200

    Frontend Adjustment

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://acddcb851cad741fabbe2e5c137344e9-539606017.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
